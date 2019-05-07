581b62addcac0d95a7969fdf1b429225e880a27f
Alejandro Fernández Martínez <fdezalejandro97@gmail.com>
2019-05-07 16:52:11 +0200

    Deshabilitados botones que no funcionan.

--- PintorLab/Views/TemplatesView.xaml.cs
+++ PintorLab/Views/TemplatesView.xaml.cs
@@ -17,13 +17,8 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace PintorLab.Views
 {
-    /// <summary>
-    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
-    /// </summary>
     public sealed partial class TemplatesView : Page
     {
         private ObservableCollection<string> current;
